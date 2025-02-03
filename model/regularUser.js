42aac0a6fa0e7105fff7a22a4cb3a799f812bedc
sachi-tmg <sachitamang43210@gmail.com>
2025-02-04 00:00:34 +0545

    linkora: other necessary fields added to regular user

--- model/regularUser.js
+++ model/regularUser.js
@@ -15,6 +15,51 @@ const regularUserSchema = new mongoose.Schema({
         type: String,
         default: "",
     },
+    social_links: {
+        youtube: {
+            type: String,
+            default: "",
+        },
+        instagram: {
+            type: String,
+            default: "",
+        },
+        facebook: {
+            type: String,
+            default: "",
+        },
+        twitter: {
+            type: String,
+            default: "",
+        },
+        github: {
+            type: String,
+            default: "",
+        },
+        website: {
+            type: String,
+            default: "",
+        }
+    },
+    account_info:{
+        total_posts: {
+            type: Number,
+            default: 0
+        },
+        total_reads: {
+            type: Number,
+            default: 0
+        },
+    },
+    google_auth: {
+        type: Boolean,
+        default: false
+    },
+    blogs: {
+        type: [ mongoose.Schema.Types.ObjectId ],
+        ref: 'posts',
+        default: [],
+    },
     dateCreated: {
         type: Date,
         default: Date.now,
