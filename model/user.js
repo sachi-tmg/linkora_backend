f1ad0d2e086fe3923c722ce190f59399361cf2ac
sachi-tmg <sachitamang43210@gmail.com>
2025-02-03 23:59:56 +0545

    linkora: username field added to user schema

--- model/user.js
+++ model/user.js
@@ -14,6 +14,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    username: {
+        type: String,
+        unique: true,
+    },
     roleId: {
         type: mongoose.Schema.Types.ObjectId, 
         ref: "roles", 
