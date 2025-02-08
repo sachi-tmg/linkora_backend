b60e348b2df5212e08c9859a5392becfcc40d510
sachi-tmg <sachitamang43210@gmail.com>
2025-02-08 15:33:47 +0545

    linkora: placing mongodb url to .env

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async ()=>{
     try {
-        await mongoose.connect("mongodb://localhost:27017/db_linkora");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB connected");
     } catch (e) {
         console.log("MongoDB not connected");
