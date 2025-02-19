4f800b905e4a18d28b836519a601d6adae121ef3
sachi-tmg <sachitamang43210@gmail.com>
2025-02-19 14:41:07 +0545

    linkora: renamed post to blog for more clear code

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const connectDB = require("./config/db")
 const user_router = require("./routes/user_route")
 const role_router = require("./routes/role_route")
 const regularUser_router = require("./routes/regularUser_route")
-const post_router = require("./routes/post_route");
+const blog_router = require("./routes/blog_route");
 const like_router = require("./routes/like_route");
 const comment_router = require("./routes/comment_route");
 const cors = require("cors");
@@ -26,9 +26,10 @@ app.use(express.json());
 app.use("/api/role", role_router);
 app.use("/api/user", user_router);
 app.use("/api/regular-users", regularUser_router);
-app.use("/api/post", post_router);
+app.use("/api/blog", blog_router);
 app.use("/api/like", like_router);
 app.use("/api/comment", comment_router);
+app.use("/public", express.static('public'));
 
 
 const port = 3000;
